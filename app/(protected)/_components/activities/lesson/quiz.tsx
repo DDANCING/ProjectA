a9a2f5f2355f0dbd2a3a55bb8a5b4ec6866a88ec
MARCELOMAZZONETTO <MARCMAKER@OUTLOOK.COM>
2024-10-01 17:15:14 -0300

    add: API challenger progress

--- app/(protected)/_components/activities/lesson/quiz.tsx
+++ app/(protected)/_components/activities/lesson/quiz.tsx
@@ -50,10 +50,43 @@ export const Quiz = ({
   const challenge = challenges[activeIndex];
   const options = challenge.challengeOptions ?? [];
 
+  const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  };
+
   const onSelect = (id: number) => {
     if (status !== "none") return;
 
     setSelectedOption(id);
+  };
+
+  const onContinue =  () => {
+    if (!selectedOption) return;
+
+    if (status === "wrong") {
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+    if (status === "correct") {
+      onNext();
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+    
+    const correctOption = options.find((option)  => option.correct);
+
+    if (!correctOption) {
+      return;
+    }
+
+    if (correctOption.id === selectedOption) {
+      console.log("correct option!");
+
+    } else {
+      console.log("wrong option!");
+    }
   };
 
   const title = challenge.type === "ASSIST" 
@@ -96,7 +129,7 @@ export const Quiz = ({
        <Footer
         disabled={!selectedOption}
         status={status}
-        onCheck={() => {}}
+        onCheck={onContinue}
         
       />
       </div>
